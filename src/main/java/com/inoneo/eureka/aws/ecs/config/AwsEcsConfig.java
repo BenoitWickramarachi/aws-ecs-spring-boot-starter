01847ff3d43d7dc5d2678e0688974c7387bcd0ff
Benoit Wickramarachi <wickramben@gmail.com>
2018-02-19 21:38:22 +0100

    Add properties to use a file path instead of the env variable for test

--- src/main/java/com/inoneo/eureka/aws/ecs/config/AwsEcsConfig.java
+++ src/main/java/com/inoneo/eureka/aws/ecs/config/AwsEcsConfig.java
@@ -3,11 +3,15 @@ package com.inoneo.eureka.aws.ecs.config;
 import com.inoneo.eureka.aws.ecs.builder.EcsContainerMetadataBuilder;
 import com.inoneo.eureka.aws.ecs.model.EcsContainerMetadata;
 import com.netflix.appinfo.AmazonInfo;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.cloud.commons.util.InetUtils;
 import org.springframework.cloud.netflix.eureka.EurekaInstanceConfigBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.Resource;
+
+import java.io.IOException;
 
 @Configuration
 @ConditionalOnProperty(prefix = "awsecs", name= "enabled", havingValue = "true")
@@ -15,9 +19,24 @@ public class AwsEcsConfig {
 
     private static final String ECS_CONTAINER_METADATA_FILE = "ECS_CONTAINER_METADATA_FILE";
 
+    @Value("${awsecs.useEnvVariable:true}")
+    private boolean useEnvVariable;
+
+    @Value("${awsecs.metadataFileName}")
+    private Resource metadataFileName;
+
     @Bean
     public EcsContainerMetadata ecsContainerMetadata() {
-        String ecsContainerMetadataFilePath = System.getenv(ECS_CONTAINER_METADATA_FILE);
+        String ecsContainerMetadataFilePath;
+        if(useEnvVariable) {
+            ecsContainerMetadataFilePath = System.getenv(ECS_CONTAINER_METADATA_FILE);
+        } else {
+            try {
+                ecsContainerMetadataFilePath = metadataFileName.getFile().getPath();
+            } catch (IOException e) {
+                throw new RuntimeException(String.format("Unable to load file at %s", metadataFileName.toString()));
+            }
+        }
         if (ecsContainerMetadataFilePath == null) {
             throw new RuntimeException(String.format("Environment variable (%s) is not set." +
                             " Verify that container metadata file generation is enable at the ECS agent level" +
